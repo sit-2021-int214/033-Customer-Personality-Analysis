e76e1218cb3b137f2ed977a516b85f18d1028f25
Pattarapon Makhirun <pattarapon.it@mail.kmutt.ac.th>
2021-10-16 18:21:13 +0700

    update

--- marketing_campaign/dataset_import.R
+++
@@ -1,5 +0,0 @@
-CPA_DATA <- read.delim("C:/Users/User/Desktop/INT214/033-Customer-Personality-Analysis/marketing_campaign/marketing_campaign.csv")
-View(CPA_DATA)
-
-write.csv(CPA_DATA,"C:/Users/User/Desktop/INT214/CPA.csv")
-

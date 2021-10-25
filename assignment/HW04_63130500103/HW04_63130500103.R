ac06dfd373568391c8b28af11db5796fe1258ec9
Will103.SIT <73602900+willvaritchai@users.noreply.github.com>
2021-10-25 16:39:48 +0700

    test-plotgraph
    test-plotgraph

--- assignment/HW04_63130500103/HW04_63130500103.R
+++ assignment/HW04_63130500103/HW04_63130500103.R
@@ -1,1 +1,37 @@
+#ทำการ add library
+library(dplyr)
+library(readr)
+
+
+#Base R
+hist(starwars$height)
+plot(x=starwars$height,y=starwars$mass)
+
+
+#Load Data
+Top_ComSci_Prog_Books <- read.csv("https://raw.githubusercontent.com/safesit23/INT214-Statistics/main/datasets/prog_book.csv")
+
+View(Top_ComSci_Prog_Books)
+
+
+#Set Data
+Count_Type <- table(Top_ComSci_Prog_Books$Type)
+
+Count_Type
+
+
+#Create Graph from "#Set Data"
+barplot(Count_Type)
+
+  #ปรับแต่งให้มีการแสดงรายละเอียดของกราฟ
+barplot(Count_Type,
+        main = "Number of books of each type", #Title of graphs
+        xlab = "Name of books", #X-axis title
+        ylab = "Number of books" #Y-axis title
+)
+
+
+
+
+
 
